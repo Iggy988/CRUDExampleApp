c2cccb8ee77fa33adba0f11c2480cf46d6a8e073
Iggy988 <igor.rodic1988@gmail.com>
2023-07-12 23:42:58 +0200

    implementing tests

--- CRUDTests/CountriesServiceTest.cs
+++ CRUDTests/CountriesServiceTest.cs
@@ -1,7 +1,7 @@
-﻿using Xunit;
-using ServiceContracts;
+﻿using ServiceContracts;
 using ServiceContracts.DTO;
 using Services;
+using Xunit;
 
 namespace CRUDTests;
 
@@ -9,11 +9,13 @@ public class CountriesServiceTest
 {
     private readonly ICountriesService _countriesService;
 
+    //constructor
     public CountriesServiceTest()
     {
         _countriesService = new CountriesService();
     }
 
+    #region AddCountry
     // When CountryAddRequest is null it should throw ArgumentNullException
     [Fact]
     public void AddCountry_NullCountry()
@@ -34,7 +36,7 @@ public class CountriesServiceTest
     public void AddCountry_CountryNameIsNull()
     {
         //Arrange
-        CountryAddRequest? request = new CountryAddRequest() { CountryName = null};
+        CountryAddRequest? request = new CountryAddRequest() { CountryName = null };
 
         //Assert
         Assert.Throws<ArgumentException>(() =>
@@ -49,11 +51,11 @@ public class CountriesServiceTest
     public void AddCountry_DuplicateContryName()
     {
         //Arrange
-        CountryAddRequest? request1 = new CountryAddRequest() { CountryName = "Serbia"};
-        CountryAddRequest? request2 = new CountryAddRequest() { CountryName = "Serbia"};
+        CountryAddRequest? request1 = new CountryAddRequest() { CountryName = "Serbia" };
+        CountryAddRequest? request2 = new CountryAddRequest() { CountryName = "Serbia" };
 
         //Assert
-        Assert.Throws<ArgumentNullException>(() =>
+        Assert.Throws<ArgumentException>(() =>
         {
             //Act
             _countriesService.AddCountry(request1);
@@ -69,10 +71,63 @@ public class CountriesServiceTest
         CountryAddRequest? request = new CountryAddRequest() { CountryName = "Japan" };
 
         //Act
-        CountryResponse response =  _countriesService.AddCountry(request);
+        CountryResponse response = _countriesService.AddCountry(request);
+        List<CountryResponse> countries_from_GetAllCountries = _countriesService.GetAllCountries();
 
         //Assert
         Assert.True(response.CountryId != Guid.Empty);
-
+        //takes equals method between 2 object
+        Assert.Contains(response, countries_from_GetAllCountries);
+        
+        //objA.Equals(objB) -> dasn't compare values
     }
+    
+    #endregion
+
+
+    #region GetAllCountries
+
+    //The list of countries should be empty by default (before adding any countries)
+    [Fact]
+    public void GetAllCountries_EmptyList()
+    {
+        //Act
+        List<CountryResponse> actual_country_response_list =
+        _countriesService.GetAllCountries();
+
+        //Assert
+        Assert.Empty(actual_country_response_list); // ako je collection list pun test je prosao
+    }
+
+    [Fact]
+    public void GetAllCountries_AddFewCountries()
+    {
+        //Arrange
+        List<CountryAddRequest> country_request_list = new List<CountryAddRequest>()
+       {
+            new CountryAddRequest() {CountryName = "USA"},
+            new CountryAddRequest() {CountryName = "Russia"},
+            new CountryAddRequest() {CountryName = "Japan"},
+       };
+        //Act
+        List<CountryResponse> countries_list_from_add_country = new List<CountryResponse>();
+
+        foreach (CountryAddRequest country_request in country_request_list)
+        {
+            countries_list_from_add_country.Add (_countriesService.AddCountry(country_request));
+        }
+        
+        List<CountryResponse> actualCountryResponseList =  _countriesService.GetAllCountries();
+
+        //read each element from countries_list_from_add_country
+        foreach(CountryResponse expected_country in countries_list_from_add_country)
+        {
+            //Assert
+            Assert.Contains(expected_country, actualCountryResponseList);
+        }
+    }
+
+
+    #endregion
+
 }
--- Services/CountriesService.cs
+++ Services/CountriesService.cs
@@ -1,12 +1,55 @@
-﻿using ServiceContracts;
+﻿using Enttities;
+using ServiceContracts;
 using ServiceContracts.DTO;
 
 namespace Services;
 
 public class CountriesService : ICountriesService
 {
+    //private field
+    private readonly List<Country> _countries;
+    //constructor
+    public CountriesService()
+    {
+        _countries = new List<Country>();
+    }
     public CountryResponse AddCountry(CountryAddRequest? countryAddRequest)
     {
-        throw new NotImplementedException();
+        //Validation: countryAddRequest parameter cant be null
+        if (countryAddRequest == null)
+        {
+            throw new ArgumentNullException(nameof(countryAddRequest));
+        }
+        
+      
+        //Validation: countryName cant be null
+        if (countryAddRequest.CountryName == null)
+        {
+            throw new ArgumentException(nameof(countryAddRequest.CountryName));
+        }
+
+        //Validation: CountryName cant be duplicate
+        if (_countries.Where(temp => temp.CountryName ==
+            countryAddRequest.CountryName).Count() > 0)
+        {
+            throw new ArgumentException("Given country name already exist");
+        }
+
+        //Convert object from CountryAddRequest to Country type
+        Country country = countryAddRequest.ToCountry();
+    
+        //generate CountryID
+        country.CountryId = Guid.NewGuid();
+
+        //Add country object into _countries
+        _countries.Add(country);
+
+        return country.ToCountryResponse();
+    }
+
+    public List<CountryResponse> GetAllCountries()
+    {
+        //converting each element from country to ToCountryResponse -> returns ToCountryResponse
+        return _countries.Select(country => country.ToCountryResponse()).ToList();
     }
 }
