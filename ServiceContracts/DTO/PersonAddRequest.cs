2e1099510a460bf942ff1a7972699670f123fcd0
Iggy988 <igor.rodic1988@gmail.com>
2023-07-14 20:55:16 +0200

    novi unos

--- ServiceContracts/DTO/PersonAddRequest.cs
+++ ServiceContracts/DTO/PersonAddRequest.cs
@@ -1,14 +1,20 @@
 ﻿using Enttities;
 using ServiceContracts.Enums;
+using System.ComponentModel.DataAnnotations;
 
 namespace ServiceContracts.DTO;
 /// <summary>
-/// Act as DTO for inserting a new person
+/// Acts as a DTO for inserting a new person
 /// </summary>
 public class PersonAddRequest
 {
+    //[Required(ErrorMessage = "Person Name can't be blank")]
     public string? PersonName { get; set; }
+
+    //[Required(ErrorMessage = "Email can't be blank")]
+    //[EmailAddress(ErrorMessage = "Email value should be a valid email")]
     public string? Email { get; set; }
+
     public DateTime? DateOfBirth { get; set; }
     public GenderOptions? Gender { get; set; }
     public Guid? CountryID { get; set; }
@@ -16,16 +22,12 @@ public class PersonAddRequest
     public bool ReceiveNewsLetters { get; set; }
 
     /// <summary>
-    /// Converts the current object of PersonAddPerson into a new object of Person type
+    /// Converts the current object of PersonAddRequest into a new object of Person type
     /// </summary>
     /// <returns></returns>
     public Person ToPerson()
     {
-        return new Person()
-        { 
-            PersonName = PersonName, Email = Email, DateOfBirth = DateOfBirth,
-            Gender = Gender.ToString(), CountryID = CountryID, Address = Address,
-            ReceiveNewsLetters = ReceiveNewsLetters
-        };
+        return new Person() { PersonName = PersonName, Email = Email, DateOfBirth = DateOfBirth, Gender = Gender.ToString(), Address = Address, CountryID = CountryID, ReceiveNewsLetters = ReceiveNewsLetters };
     }
 }
+
