0f349f78b82c7b6e2bb01fd9caa3265d06172200
Iggy988 <igor.rodic1988@gmail.com>
2023-08-14 23:01:00 +0200

    Update UI

--- CRUDExample/Filters/ActionFilters/PersonsListActionFilter.cs
+++ CRUDExample/Filters/ActionFilters/PersonsListActionFilter.cs
@@ -1,6 +1,7 @@
 ﻿using CRUDExample.Controllers;
 using Microsoft.AspNetCore.Mvc.Filters;
 using ServiceContracts.DTO;
+using ServiceContracts.Enums;
 
 namespace CRUDExample.Filters.ActionFilters;
 
@@ -76,10 +77,18 @@ public class PersonsListActionFilter : IActionFilter
             {
                 personsController.ViewData["CurrentSortBy"] = Convert.ToString(parameters["sortBy"]);
             }
+            else
+            {
+                personsController.ViewData["CurrentSortBy"] = nameof(PersonResponse.PersonName);
+            }
 
             if (parameters.ContainsKey("sortOrder"))
             {
                 personsController.ViewData["CurrentSortOrder"] = Convert.ToString(parameters["sortOrder"]);
+            }
+            else
+            {
+                personsController.ViewData["CurrentSortOrder"] = nameof(SortOrderOptions.ASC);
             }
         }
 
